5af8e3cedd2235df33f2128f451790839d1ee8f7
Fadia Saied <fadiasaied01110@gmail.com>
2023-09-28 22:11:13 +0300

    edit pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingRoutingModule } from './app-routing-routing.module';
 import { SharedModule } from './shared/shared.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { HomepageComponent } from './homepage/homepage.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DropDownComponent } from './components/drop-down/drop-down.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SearchFormComponent } from './components/search-form/search-form.component';
 import { PharmacyDetailsComponent } from '../pharmacy/pharmacy-details/pharmacy-details.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
