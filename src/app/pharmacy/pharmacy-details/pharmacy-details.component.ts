6526c2568494d2298ca1e03a37ae6f8e28da5219
“MhmdMahmoud5” <bigvirus800@gmail.com>
2023-09-28 03:58:02 +0300

    try to enhance pharmacy-detailes componant , add card and service

--- src/app/pharmacy/pharmacy-details/pharmacy-details.component.ts
+++ src/app/pharmacy/pharmacy-details/pharmacy-details.component.ts
@@ -7,11 +7,11 @@ import { ServiceService } from 'src/app/shared/services/service.service';
   styleUrls: ['./pharmacy-details.component.css']
 })
 export class PharmacyDetailsComponent implements OnInit {
-  name: any;
-  address: any;
-  rating: any;
-  number:any;
-  image:any;
+  name: any="a";
+  address: any="bens";
+  rating: Number=2.4;
+  number:any="+2 5215821";
+  image:any="https://t4.ftcdn.net/jpg/00/89/89/91/360_F_89899105_UN5Bv2hYUx0TFzBdwpi8K1rkPzl3dYLx.jpg";
   constructor(private service: ServiceService){}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
