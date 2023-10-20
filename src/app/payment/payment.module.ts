eb7793382c2a4fa74d11b5b936e629a5ccb2632f
peter <petersamy165@gmail.com>
2023-10-20 11:57:59 +0300

    create pharmacist model

---
+++ src/app/payment/payment.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaymentComponent } from './components/payment/payment.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaymentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaymentModule { }
---
+++ src/app/pharmacist-home/pharmacist-home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddProductsComponent } from './components/products/add-products/add-products.component';
+import { EditProductsComponent } from './components/edit-products/edit-products.component';
+import { ViewOrdersComponent } from './components/view-orders/view-orders.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListProductsComponent,
+    AddProductsComponent,
+    EditProductsComponent,
+    ViewOrdersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PharmacistHomeModule { }
