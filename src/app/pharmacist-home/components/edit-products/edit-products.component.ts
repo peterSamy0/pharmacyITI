9c27f6aebc7d5e29b5814ea5f602277a81bb5142
Nehal-Mahdy <nehalhassan914@yahoo.com>
2023-10-26 01:25:04 +0300

    edit_product

--- src/app/pharmacist-home/components/edit-products/edit-products.component.ts
+++ src/app/pharmacist-home/components/edit-products/edit-products.component.ts
@@ -11,11 +11,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class EditProductsComponent {
   constructor (private activeRoute : ActivatedRoute){
       this.updateProductForm = new FormGroup({
-        productName: new FormControl('', [Validators.required]),
-        productQuantity: new FormControl('',[Validators.required]),
+        
+        productStock: new FormControl('',[Validators.required]),
         productPrice: new FormControl('',[Validators.required]),
-        productExpire: new FormControl('',[Validators.required]),
-        productCategory: new FormControl('',[Validators.required])
   
   
   
@@ -36,19 +34,15 @@ export class EditProductsComponent {
   
 update(){
 console.log(this.updateProductForm.value);
-let product_name =this.updateProductForm.controls['productName'].value;
-let product_quantity =this.updateProductForm.controls['productQuantity'].value;
 let product_Price =this.updateProductForm.controls['productPrice'].value;
-let product_Expire =this.updateProductForm.controls['productExpire'].value;
-let product_Category =this.updateProductForm.controls['productCategory'].value;
+let product_stock =this.updateProductForm.controls['productStock'].value;
 
 let updatedProduct= {
 id : this.activeRoute.snapshot.params['id'] ,
-productName: product_name,
-productQuantity: product_quantity,
+productName: this.productId.name,
+productStock: product_stock,
 productPrice: product_Price,
-productExpire: product_Expire,
-productCategory: product_Category,
+productCategory: this.productId.Category,
 };
 localStorage.setItem("currentPharma", JSON.stringify(updatedProduct));
       // console.log(this.updatedProduct);
