e9ac31de32bc5611ddd304deff3506fcac426972
“MhmdMahmoud5” <bigvirus800@gmail.com>
2023-10-26 18:27:43 +0300

    make a simple payment form in html,ts,service,module

--- src/app/payment/components/payment/payment.component.ts
+++ src/app/payment/components/payment/payment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaymentService } from '../../services/payment.service';
 
 @Component({
   selector: 'app-payment',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent {
+  paymentData: any = {}; // Object to store payment form data
 
+  constructor(private paymentService: PaymentService) { }
+
+  onSubmit(paymentForm: NgForm) {
+    if (paymentForm.valid) {
+      this.paymentService.processPayment(this.paymentData)
+        .then(paymentId => {
+          console.log('Payment successful. Payment ID:', paymentId);
+          // Add any additional logic or navigation upon successful payment
+        })
+        .catch(error => {
+          console.error('Payment failed:', error);
+          // Add any error handling or display error message to the user
+        });
+    }
+  }
 }
