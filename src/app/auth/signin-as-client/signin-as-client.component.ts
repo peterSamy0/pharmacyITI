9f3025c049ad138b9f02781654bd43cc64b89487
peter <abanoub.samy.mikhail@gmail.com>
2023-11-03 19:21:51 +0200

    edit sign in as client

--- src/app/auth/signin-as-client/signin-as-client.component.ts
+++ src/app/auth/signin-as-client/signin-as-client.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
 import { Users } from 'src/app/interface/users';
 import userData from '../../../assets/json/users.json'
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import Swal, { SweetAlertIcon } from 'sweetalert2'
 
+// CommonJS
 @Component({
   selector: 'app-signin-as-client',
   templateUrl: './signin-as-client.component.html',
@@ -13,8 +17,10 @@ export class SigninAsClientComponent {
 
   users : Users[]= userData;
 
+  Swal !:SweetAlertIcon;
+
   signinForm: FormGroup;
-  constructor() {
+  constructor(private http: HttpClient, private router: Router) {
     this.signinForm = new FormGroup({
       userEmail: new FormControl('', [Validators.required]),
       userPass: new FormControl('',[Validators.required])
@@ -23,26 +29,32 @@ export class SigninAsClientComponent {
     }
 
     checkUser(){
-      console.log(this.signinForm);
-      // console.log(this.signinForm.controls['userEmail'].value);
-     let userEmail =this.signinForm.controls['userEmail'].value;
-     let userPass =this.signinForm.controls['userPass'].value;
-     
-     for (var i = 0; i < this.users.length; i++) {
-      if (
-        userEmail === this.users[i].email &&
-        userPass == this.users[i].userPass
-      ) {
-        console.log('correct')
-     
-        localStorage.setItem("currentUser", JSON.stringify(this.users[i]));
-        
-        return;
-      }
-      
-}
- 
+      let userEmail =this.signinForm.controls['userEmail'].value;
+      let userPass =this.signinForm.controls['userPass'].value;
+      const body = {
+        "email": userEmail,
+        "password":   userPass     
+      } 
+
+      this.http.post(`http://localhost:8000/api/auth/login`, body)
+        .subscribe(
+          response => {
+            console.log(response);
+            this.router.navigate(['/']);
+          },
+  
+          error => {
+            console.log(error)
+            Swal.fire({
+              title: 'Error!',
+              text: 'invaled email or password',
+              icon: 'error',
+              confirmButtonText: 'Cool'
+            })
+          }
+        );
     }
+
    
   }
 
