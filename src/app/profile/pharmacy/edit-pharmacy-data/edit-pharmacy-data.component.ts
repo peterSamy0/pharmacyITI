3341e7c2d5ee2284682c18aeac1b54fcd695c70e
peter <abanoub.samy.mikhail@gmail.com>
2023-11-07 03:33:38 +0200

    complete edit of pharmacy

--- src/app/profile/pharmacy/edit-pharmacy-data/edit-pharmacy-data.component.ts
+++ src/app/profile/pharmacy/edit-pharmacy-data/edit-pharmacy-data.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import pharmaciesData from '../../../../assets/json/pharmcies.json';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import Swal, { SweetAlertIcon } from 'sweetalert2'
+
+
 @Component({
   selector: 'app-edit-pharmacy-data',
   templateUrl: './edit-pharmacy-data.component.html',
   styleUrls: ['./edit-pharmacy-data.component.css']
 })
+
+
 export class EditPharmacyDataComponent {
   updatePharmaForm: FormGroup;
   pharmacies: Array<any> = pharmaciesData;
@@ -15,39 +21,77 @@ export class EditPharmacyDataComponent {
   passFail: boolean = false;
   userFullNameFail: boolean = false;
   notAllDataEntered: boolean = false;
-
-  constructor(private activeRoute: ActivatedRoute, private router: Router) {
+  Swal !:SweetAlertIcon;
+  isCity : boolean = false;
+  governorateID !: number;
+  cities : any;
+  governorates : any;
+  cityID !: number;
+  daysArr: any = [];
+  days: any;
+  selectedDayId: number = 0;
+  selectedDayName: string = '';
+  selectedDays: { id: string, day: string }[] = [];
+  id :any;
+  constructor(private activeRoute: ActivatedRoute, private router: Router, private http: HttpClient) {
     this.updatePharmaForm = new FormGroup({
       pharmaName: new FormControl('', [Validators.required]),
       pharmaPhone: new FormControl('', [Validators.required]),
       pharmaEmail: new FormControl('', [Validators.required]),
-      pharmaCity: new FormControl('', [Validators.required]),
-      pharmaGovern: new FormControl('', [Validators.required]),
       pharmaPass: new FormControl('', [Validators.required]),
       pharmaLicense:  new FormControl('', [Validators.required]),
+      pharmaBankAccount:  new FormControl('', [Validators.required]),
+      pharmaOpeningTime:  new FormControl('', [Validators.required]),
+      pharmaClosingTime:  new FormControl('', [Validators.required]),
+      pharmaStreet:  new FormControl('', [Validators.required]),
       avaliableToDeliver: new FormControl('', [Validators.required]),
     });
   }
 
   ngOnInit() {
-    console.log(this.activeRoute.snapshot.params['id']);
+    this.id = this.activeRoute.snapshot.params['id']
+    this.getGovernorates();
+    this.getDays()
   }
 
   pharmaId: any = this.pharmacies[this.activeRoute.snapshot.params['id'] - 1];
   update() {
-    console.log(this.updatePharmaForm.value);
+
     let pharmaName =this.updatePharmaForm.controls['pharmaName'].value;
     let pharmaEmail = this.updatePharmaForm.controls['pharmaEmail'].value;
-    let pharmaLicense =this.updatePharmaForm.controls['pharmaLicense'].value;
-    let pharmaPhone = this.updatePharmaForm.controls['pharmaPhone'].value;
-    let pharmaCity = this.updatePharmaForm.controls['pharmaCity'].value;
     let pharmaPass = this.updatePharmaForm.controls['pharmaPass'].value;
-    let pharmaGovern = this.updatePharmaForm.controls['pharmaGovern'].value;
+    let pharmaStreet =this.updatePharmaForm.controls['pharmaStreet'].value;
+    let pharmaLicense =this.updatePharmaForm.controls['pharmaLicense'].value;
+    let pharmaBankAccount =this.updatePharmaForm.controls['pharmaBankAccount'].value;
+    let pharmaPhone = this.updatePharmaForm.controls['pharmaPhone'].value;
+    let pharmaOpeningTime = this.updatePharmaForm.controls['pharmaOpeningTime'].value;
+    let pharmaClosingTime = this.updatePharmaForm.controls['pharmaClosingTime'].value;
     let availability =this.updatePharmaForm.controls['avaliableToDeliver'].value;
-
     let emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     let passPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
+    const body = {
+      "user" : {
+        "name" : pharmaName,
+        "email": pharmaEmail,
+        "password": pharmaPass     
+      },
+      "pharmacy" : {
+        "image" : "test.png",
+        "licence_number": pharmaLicense,
+        "opening": pharmaOpeningTime,
+        "closing": pharmaClosingTime,
+        "street": pharmaStreet,
+        "bank_account": +pharmaBankAccount || null,
+        "governorate_id" : this.governorateID,
+        "city_id" : this.cityID
+      },
+      "daysOff": this.daysArr
+    }
+
+    console.log(body)
+
+    
     if (!pharmaEmail.match(emailPattern)) {
       console.log('invalid email format');
       this.emailFail = true;
@@ -57,9 +101,7 @@ export class EditPharmacyDataComponent {
     }  else if (
       pharmaEmail &&
       pharmaLicense&&
-      pharmaGovern &&
       pharmaPhone&&
-      pharmaCity &&
       pharmaName &&
       // deliveryPass &&
       availability
@@ -68,8 +110,7 @@ export class EditPharmacyDataComponent {
         id: this.activeRoute.snapshot.params['id'],
         pharmacyName: pharmaName,
         password: pharmaPass,
-        city: pharmaCity,
-        governorate: pharmaGovern,
+        // city: pharmaCity,
         email:pharmaEmail,
         phone: pharmaPhone,
         licenseNum: pharmaLicense       
@@ -84,5 +125,72 @@ export class EditPharmacyDataComponent {
     } else {
       this.notAllDataEntered = true;
     }
+
+    this.http.put(`http://localhost:8000/api/pharmacies/${this.id}`, body)
+      .subscribe(
+        response => {
+          this.router.navigate(['/']);
+        },
+
+        error => {
+          console.log(error)
+          Swal.fire({
+            title: 'Error!',
+            text: 'invaled data',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })
+    });
   }
+
+  selectedGov(val: any){
+    this.isCity = true
+    this.governorateID = val;
+    this.http.get(`http://localhost:8000/api/governorates/${val}`)
+      .subscribe(
+        response => {
+          this.cities = response;
+        },
+        error => console.log(error)
+      )
+  }
+
+  getGovernorates(){
+    this.http.get(`http://localhost:8000/api/governorates`)
+      .subscribe(
+        response => {
+          this.governorates = response;
+          console.log(this.governorates)
+        },
+        error => console.log(error)
+      )
+  }
+  selectedCity(val: any){
+    this.cityID = val;
+  }
+
+
+  getDays(){
+    this.http.get(`http://localhost:8000/api/days`)
+      .subscribe(
+        response => {
+          this.days = response
+        },
+        error => console.log(error)
+      )
+  }
+
+  chooseDay(val:any){
+    const selectedDay = this.days.data.find((day: any) => day.id == val);
+    const isExists = this.daysArr.includes(+val);
+    if(!isExists && selectedDay){
+      this.selectedDays.push(selectedDay);
+      this.daysArr.push(+val);
+    }
+  }
+  removeDay(val:any){
+    this.selectedDays = this.selectedDays.filter( (item:any) => item.id != val)
+    this.daysArr = this.daysArr.filter( (item:any) => item.id != val)
+  }
+
 }
