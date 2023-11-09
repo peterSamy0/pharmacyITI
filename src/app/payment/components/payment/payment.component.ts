f2bb55751c2c22b9b76754b37936efd858f2b000
“MhmdMahmoud5” <bigvirus800@gmail.com>
2023-11-09 18:04:18 +0200

    enhance in the code

--- src/app/payment/components/payment/payment.component.ts
+++ src/app/payment/components/payment/payment.component.ts
@@ -1,76 +1,76 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StripeService } from 'ngx-stripe'; // Removed unnecessary import
 import { HttpClient } from '@angular/common/http';
-import { loadStripe, Stripe, StripeElements, StripeCardElement } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent {
-  @ViewChild('cardElement')
-  cardElement!: ElementRef;
-  paymentForm: FormGroup;
-  stripe: Stripe | null = null;
-  card: StripeCardElement | null = null;
+export class PaymentComponent implements OnInit {
+  paymentStatus: string = '';
+  paymentForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(
+    private fb: FormBuilder,
+    private stripeService: StripeService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
     this.paymentForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      name: ['', Validators.required],
-      exp: ['', Validators.required], // Add this line for the 'exp' field
-      cvv: ['', Validators.required], // Add this line for the 'cvv' field
-
+      cardHolder: ['', Validators.required],
+      cardNumber: ['', Validators.required],
+      exp: ['', Validators.required],
+      cvv: ['', Validators.required]
     });
   }
 
-  async ngOnInit() {
-    this.stripe = await loadStripe('pk_test_51OAJmQIjYA1iuKSADjQU12ZvCU2S51PFLNZjJutxbNbkxL2VL26zr8GVdlC9f1uTiE5ZyNFRPbdoGUqOshCFQ9LR00sDAlY7vq');
-  }
-
-  async ngAfterViewInit() {
-    if (this.stripe) {
-      const elements: StripeElements = this.stripe.elements();
-      this.card = elements.create('card', {
-        style: {
-          base: {
-            fontSize: '16px',
-          },
-        },
-      });
-  
-      if (this.card) {
-        this.card.mount(this.cardElement.nativeElement);
-      } else {
-        console.error('Stripe Card Element is null. Check your Stripe setup.');
-      }
-    } else {
-      console.error('Stripe is not initialized. Check your Stripe setup.');
-    }
-  }
-
   async submitPayment() {
-    try {
-      const stripe = this.stripe;
-      if (stripe && this.card) {
-        const { token, error } = await stripe.createToken(this.card);
-
-        if (error) {
-          console.error(error);
+    const card = this.paymentForm.get('cardNumber')!.value;
+    
+    this.stripeService.createToken(card).subscribe(
+      (tokenResult: any) => {
+        const token = tokenResult?.token?.id;
+  
+        if (token) {
+          const formData = {
+            email: this.paymentForm.get('email')!.value,
+            cardHolder: this.paymentForm.get('cardHolder')!.value,
+            token: token
+          };
+  
+          this.http.post('http://localhost:8000/api/process-payment', formData)
+            .subscribe(
+              (response: any) => {
+                // Handle successful response
+                this.paymentStatus = response.message;
+  
+                // Display order status to the user
+                if (response.orderStatus === 'Order Verified and Validated') {
+                  this.paymentStatus += ' - ' + response.orderStatus;
+                } else {
+                  this.paymentStatus += ' - ' + response.orderStatus;
+                }
+              },
+              (error) => {
+                // Handle error response
+                console.error(error);
+                this.paymentStatus = 'Error processing payment.';
+              }
+            );
         } else {
-          this.http.post('http://localhost:8000/api/payment/process', { token: token.id, secretKey: 'sk_test_51OAJmQIjYA1iuKSAYbGwvYX1eWgHH0nbJaHqeq8s9nFpEfNTA1iu0SjpLISNTU7TGBqiD11Jt6M9zGsHu3rfcjrD005tMFIUac' }).subscribe(
-            (response) => {
-              console.log('Payment successful', response);
-              // Redirect to the order status page
-            },
-            (error) => {
-              console.error('Payment error', error);
-            }
-          );
+          // Handle the case where the token is not available
+          console.error('Token not available');
         }
+      },
+      (error) => {
+        // Handle error during token creation
+        console.error(error);
+        this.paymentStatus = 'Error processing payment.';
       }
-    } catch (error) {
-      console.error(error);
-    }
+    );
   }
 }
