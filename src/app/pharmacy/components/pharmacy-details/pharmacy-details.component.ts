b7a52c0a5cb97c9ab5d424ce1111a4f7acba4a0b
peter <abanoub.samy.mikhail@gmail.com>
2023-11-20 23:18:58 +0200

    fixing phone number

--- src/app/pharmacy/components/pharmacy-details/pharmacy-details.component.ts
+++ src/app/pharmacy/components/pharmacy-details/pharmacy-details.component.ts
@@ -45,10 +45,11 @@ export class PharmacyDetailsComponent {
   getPhone(val:any){
     if(val.pharmacy_phone > 0){
       this.phone = val.pharmacy_phone[0]['phone']
+    }else{
+      this.phone = 'not available now';
+      return this.phone;
     }
-    this.phone = 'not available now';
     console.log('no')
-    return this.phone;
   }
 }
 
